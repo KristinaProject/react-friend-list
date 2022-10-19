f0925a0865a7f33d26d72e20854dcbe475ae0e82
KristinaProject <kristtule@gmail.com>
2022-10-19 20:27:08 +0300

    making more class

--- friend-list/src/Friends.js
+++ friend-list/src/Friends.js
@@ -1,22 +1,53 @@
 import React from "react";
 
 class Friends extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">Name: {this.props.name}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              Surname: {this.props.surname}
+            </h6>
+            <p className="card-text">
+              Age: {this.props.age}
+              City: {this.props.city}
+            </p>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+class Container extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      surname: "",
-      age: "",
+      friends: [
+        {
+          friend: friendList,
+        },
+      ],
     };
   }
 
   render() {
     return (
       <>
-        <div>
-          <h5>Name: {this.state.name}</h5>
-          <h6>Surname: {this.state.surname}</h6>
-          <p>Age: {this.state.age}</p>
+        <div className="container">
+          {this.state.friends.map((friend) => (
+            <Friends
+              name={friend.name}
+              surname={friend.surname}
+              age={friend.age}
+              city={friend.city}
+              key={friend.id}
+            />
+          ))}
         </div>
       </>
     );
@@ -24,8 +55,8 @@ class Friends extends React.Component {
 }
 
 const friendList = [
-  { name: "Liza", surname: "Simpsons", age: 8, key: 1 },
-  { name: "Molis", surname: "Vidurzemis", age: 1000, key: 2 },
+  { name: "Liza", surname: "Simpsons", age: 8, city: "Kaunas", id: 1 },
+  { name: "Molis", surname: "Vidurzemis", age: 1000, city: "Zeme", id: 2 },
 ];
 
-export { Friends };
+export { Container };
--- friend-list/src/index.js
+++ friend-list/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Friends } from './Friends';
+import { Container } from './Friends';
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <h1>Friends</h1>
-  <Friends />
+  <h1 className='text-center'>Friends</h1>
+  <Container />
   </>
 );
 
