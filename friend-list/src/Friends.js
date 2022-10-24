ea8be9960b5624f0fc30191f33fee2a5f4d214da
KristinaProject <kristtule@gmail.com>
2022-10-24 18:22:05 +0300

    add some changes

--- friend-list/src/Friends.js
+++ friend-list/src/Friends.js
@@ -1,28 +1,28 @@
 import React from "react";
 
-class Friends extends React.Component {
-  render() {
-    return (
-      <>
-        {this.props.friends.map((friend) => (
-          <div key={friend.id} className="card m-2 p-2" style={{ width: "18rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">Name: {friend.name}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">
-                Surname: {friend.surname}
-              </h6>
-              <p className="card-text">
-                Age: {friend.age} City: {friend.city}
-              </p>
-              <button className="btn btn-primary m-1">Edit</button>
-              <button className="btn btn-danger m-1">Delete</button>
-            </div>
-          </div>
-        ))}
-      </>
-    );
-  }
-}
+// class Friends extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         {this.props.friends.map((friend) => (
+//           <div key={friend.id} className="card m-2 p-2" style={{ width: "18rem" }}>
+//             <div className="card-body">
+//               <h5 className="card-title">Name: {friend.name}</h5>
+//               <h6 className="card-subtitle mb-2 text-muted">
+//                 Surname: {friend.surname}
+//               </h6>
+//               <p className="card-text">
+//                 Age: {friend.age} City: {friend.city}
+//               </p>
+//               <button className="btn btn-primary m-1">Edit</button>
+//               <button className="btn btn-danger m-1">Delete</button>
+//             </div>
+//           </div>
+//         ))}
+//       </>
+//     );
+//   }
+// }
 
 class Container extends React.Component {
   constructor(props) {
@@ -36,13 +36,30 @@ class Container extends React.Component {
     return (
       <>
         <div className="container d-flex flex-wrap">
-          <Friends friends={this.state.friends} />
+          {this.state.friends.map((friend) => (
+            <div
+              key={friend.id}
+              className="card m-2 p-2"
+              style={{ width: "18rem" }}
+            >
+              <div className="card-body">
+                <h5 className="card-title">Name: {friend.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Surname: {friend.surname}
+                </h6>
+                <p className="card-text">
+                  Age: {friend.age} City: {friend.city}
+                </p>
+                <button className="btn btn-primary m-1">Edit</button>
+                <button className="btn btn-danger m-1">Delete</button>
+              </div>
+            </div>
+          ))}
         </div>
       </>
     );
   }
 }
-
 
 const friendList = [
   { name: "Liza", surname: "Simpsons", age: 8, city: "Kaunas", id: 1 },
